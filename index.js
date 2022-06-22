703b1059a893043659d1221159eaa88eb7431241
James Chemobo <jkchemobo@gmail.com>
2022-06-22 14:55:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+let saturdayFun = (activity = 'roller-skate') => {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = (activity = 'go to the office') => {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = (activity = '*') => {
+    return (wad = 'special') => {
+        return `You are ${activity}${wad}${activity}!`
+    }
+}
